9c47006fbb579c8df46093be1e88ae51d7f060be
Pedro Gutieres <pedro-gabriel1@hotmail.com>
2021-07-19 22:43:44 -0300

    finalizado layout tela de cadastro de evento

--- src/view/cadastro-evento/index.js
+++ src/view/cadastro-evento/index.js
@@ -12,10 +12,77 @@ import "firebase/auth";
 import Navbar from "../../components/navbar";
 
 function CadastroEvento() {
+    const [msgTipo, setMsgTipo] = useState("sucesso");
     return (
         <>
             <Navbar />
-            <h1>Cadastro evento</h1>
+            <div className="col-12 mt-5">
+                <div className="row">
+                    <h3 className="mx-auto font-weight-bold ">Novo Evento</h3>
+                </div>
+
+                <form>
+                    <div className="form-group">
+                        <label>Titulo:</label>
+                        <input type="text" className="form-control" />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Tipo do Evento:</label>
+                        <select className="form-control">
+                            <option disabled selected value>
+                                -- Selecione um tipo --
+                            </option>
+                            <option>Festa</option>
+                            <option>Teatro</option>
+                            <option>Show</option>
+                            <option>Evento</option>
+                        </select>
+                    </div>
+
+                    <div className="form-group">
+                        <label>Descrição do evento:</label>
+                        <textarea className="form-control" rows="3" />
+                    </div>
+
+                    <div className="form-group row">
+                        <div className="col-6">
+                            <label>Data:</label>
+                            <input type="date" className="form-control" />
+                        </div>
+                        <div className="col-6">
+                            <label>Hora:</label>
+                            <input type="time" className="form-control" />
+                        </div>
+                    </div>
+
+                    <div className="form-group">
+                        <label>Upload da foto:</label>
+                        <input type="file" className="form-control" />
+                    </div>
+
+                    <button
+                        type="button"
+                        className="btn btn-lg btn-block mt-3 mb-5 btn-cadastro"
+                    >
+                        Publicar evento
+                    </button>
+                </form>
+
+                <div className="msg-login text-center mt-2">
+                    {msgTipo === "sucesso" && (
+                        <span>
+                            <strong>Wow!</strong> Evento publicado! &#129304;
+                        </span>
+                    )}
+                    {msgTipo === "erro" && (
+                        <span>
+                            <strong>Ops!</strong> Não foi possível publicar o evento!
+                            &#128078;
+                        </span>
+                    )}
+                </div>
+            </div>
         </>
     );
 }
