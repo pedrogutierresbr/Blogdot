05d80822a6b43cf6f380c41b8d2a788757d3a3ea
Pedro Gutieres <pedro-gabriel1@hotmail.com>
2021-07-21 19:53:59 -0300

    refinado carregamento da pagina detalhes colocando uma condicional de mostrar conteudo somente se toda a requisicao de infos estiver carregada, senao mostra spinner

--- src/view/detalhes-evento/index.js
+++ src/view/detalhes-evento/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
+import { Spinner } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 import "./detalhes-evento.css";
@@ -13,6 +14,8 @@ import Navbar from "../../components/navbar";
 function DetalhesEvento(props) {
     const [evento, setEvento] = useState({}); //tem que ter o {} porque o firebase devolve uma estrutura JSON
     const [urlImg, setUrlImg] = useState({}); //para guardar a url da imagem que esta no firestore
+    const [carregando, setCarregando] = useState(1);
+
     const usuarioLogado = useSelector((state) => state.usuarioEmail);
 
     //Pra toda hora que carregar a tela ele ir la no firebase e carregar as infos em tela
@@ -30,70 +33,83 @@ function DetalhesEvento(props) {
                     .storage()
                     .ref(`imagens/${evento.foto}`)
                     .getDownloadURL()
-                    .then((url) => setUrlImg(url));
+                    .then((url) => {
+                        setUrlImg(url);
+                        setCarregando(0);
+                    });
             });
     }, [evento.foto, props.match.params.id]);
 
     return (
         <>
             <Navbar />
-            <div className="container-fluid ">
-                <div className="row">
-                    <img src={urlImg} className="img-banner" alt="Banner" />
-
-                    <div className="col-12 text-right mt-2 visualizacoes">
-                        <i class="fas fa-eye"></i> <span>{evento.visualizacoes}</span>
+            <div className="container-fluid">
+                {carregando ? (
+                    <div className="row  mt-5">
+                        <Spinner animation="border" variant="danger" role="status" className="mx-auto">
+                            <span className="sr-only"></span>
+                        </Spinner>
                     </div>
-
-                    <h2 className="mx-auto mt-4 titulo">
-                        <strong>{evento.titulo}</strong>
-                    </h2>
-                </div>
-
-                <div className="row mt-5 d-flex justify-content-around">
-                    <div className="col-md-3 col-sm-12 box-info p-3 my-2">
-                        <i className="fas fa-ticket-alt fa-2x"></i>
-                        <h5>
-                            <strong>Tipo</strong>
-                        </h5>
-                        <span className="mt-3">{evento.tipo}</span>
-                    </div>
-
-                    <div className="col-md-3 col-sm-12 box-info p-3 my-2">
-                        <i className="fas fa-calendar-alt fa-2x"></i>
-                        <h5>
-                            <strong>Data</strong>
-                        </h5>
-                        <span className="mt-3">{evento.data}</span>
-                    </div>
-
-                    <div className="col-md-3 col-sm-12 box-info p-3 my-2">
-                        <i className="fas fa-clock fa-2x"></i>
-                        <h5>
-                            <strong>Hora</strong>
-                        </h5>
-                        <span className="mt-3">{evento.hora}</span>
-                    </div>
-                </div>
-
-                <div className="row box-detalhes mt-5">
-                    <div className="col-12 text-center">
-                        <h5>
-                            <strong>Detalhes do Evento</strong>
-                        </h5>
-                    </div>
-
-                    <div className="col-12 text-center">
-                        <p className="text-justify py-3 px-5">{evento.detalhes}</p>
-                    </div>
-                </div>
-
-                {usuarioLogado === evento.usuario ? (
-                    <Link to="/" className="btn-editar">
-                        <i className="fas fa-pen-square fa-3x"></i>
-                    </Link>
                 ) : (
-                    ""
+                    <div>
+                        <div className="row">
+                            <img src={urlImg} className="img-banner" alt="Banner" />
+
+                            <div className="col-12 text-right mt-2 visualizacoes">
+                                <i class="fas fa-eye"></i> <span>{evento.visualizacoes}</span>
+                            </div>
+
+                            <h2 className="mx-auto mt-4 titulo">
+                                <strong>{evento.titulo}</strong>
+                            </h2>
+                        </div>
+
+                        <div className="row mt-5 d-flex justify-content-around">
+                            <div className="col-md-3 col-sm-12 box-info p-3 my-2">
+                                <i className="fas fa-ticket-alt fa-2x"></i>
+                                <h5>
+                                    <strong>Tipo</strong>
+                                </h5>
+                                <span className="mt-3">{evento.tipo}</span>
+                            </div>
+
+                            <div className="col-md-3 col-sm-12 box-info p-3 my-2">
+                                <i className="fas fa-calendar-alt fa-2x"></i>
+                                <h5>
+                                    <strong>Data</strong>
+                                </h5>
+                                <span className="mt-3">{evento.data}</span>
+                            </div>
+
+                            <div className="col-md-3 col-sm-12 box-info p-3 my-2">
+                                <i className="fas fa-clock fa-2x"></i>
+                                <h5>
+                                    <strong>Hora</strong>
+                                </h5>
+                                <span className="mt-3">{evento.hora}</span>
+                            </div>
+                        </div>
+
+                        <div className="row box-detalhes mt-5">
+                            <div className="col-12 text-center">
+                                <h5>
+                                    <strong>Detalhes do Evento</strong>
+                                </h5>
+                            </div>
+
+                            <div className="col-12 text-center">
+                                <p className="text-justify py-3 px-5">{evento.detalhes}</p>
+                            </div>
+                        </div>
+
+                        {usuarioLogado === evento.usuario ? (
+                            <Link to="/" className="btn-editar">
+                                <i className="fas fa-pen-square fa-3x"></i>
+                            </Link>
+                        ) : (
+                            ""
+                        )}
+                    </div>
                 )}
             </div>
         </>
